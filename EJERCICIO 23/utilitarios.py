5aae571687e655618b755563979e30b26e5e475a
IDIC <alumnos@alumnosuio.ute.edu.ec>
2024-12-03 20:02:07 -0500

    Cambie de todo

--- EJERCICIO 23/utilitarios.py
+++ EJERCICIO 23/utilitarios.py
@@ -2,3 +2,22 @@ def calcularPromedio(val1,val2,val3):
     calculo=val1+val2+val3
     promedio1=calculo/3
     return promedio1
+
+def calcularSubtotal(precio,cantidad,porcentaje):
+    subtotalsindescuento = precio*cantidad
+    descuento= (subtotalsindescuento*porcentaje)/100
+    subtotal= subtotalsindescuento-descuento
+    return subtotal
+
+def calcularValorHora(val1):
+    resultado= val1/160
+    return resultado
+
+def calcularTipoMulta(tipoMulta):
+    
+    return
+
+def calcularValorDescuento(precio,porcentajedescuento):
+    valorDescuento = precio*porcentajedescuento
+    resultado= valorDescuento/100
+    return resultado
